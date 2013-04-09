28e668a6421fac125f73cfd92855d2f385149a34
pyrokinetic <pyrokinetiq@powerindesign.com>
2013-04-09 10:25:25 +1000

    Updated load library to use reflection class when initialising models and controllers so you can pass parameters to the controller a la:
        $this->load->model('modelName', 'parameter1', 'parameter2');
    
    Will run the constructor like:
    
        __construct('parameter1', 'parameter2');
    
    Which is essentially the same as:
    
        $model = new modelName('parameter1', 'parameter2');

--- libraries/load.lib.php
+++ libraries/load.lib.php
@@ -6,17 +6,28 @@
 
 	public function model($model) {
 		if(!class_exists($model)) require('models/' . $model . '.php');
-		return new $model();
+		
+		$parameters = func_get_args();
+		unset($parameters[0]);
+		
+		$reflect  = new ReflectionClass($model);
+		return $reflect->newInstanceArgs($parameters);
 	}
 
 	public function controller($controller) {
 		if (!file_exists('controllers/' . $controller . '.php')) {
 			header('HTTP/1.1 404 Not Found');
-			header('Location: ' . $this->router->base . 'error/?e=404&file=' . urlencode($this->request));
+			header('Location: ' . $this->router->base . 'error/?e=404&file=' . urlencode($this->router->request));
 			return false;
 		}
+		
 		if(!class_exists($controller)) require('controllers/' . $controller . '.php');
-		return new $controller();
+		
+		$parameters = func_get_args();
+		unset($parameters[0]);
+		
+		$reflect  = new ReflectionClass($controller);
+		return $reflect->newInstanceArgs($parameters);
 	}
 
 }
